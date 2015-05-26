b15b8248056a33bb028d8fadb4786fa7aba56c49
Reed Trevelyan <reedtrevelyan@gmail.com>
2015-05-25 23:56:34 -0400

    asdf

--- IM Client v2/main/IMServer.java
+++ IM Client v2/main/IMServer.java
@@ -145,10 +145,10 @@ public class IMServer implements Runnable {
 
 			String checker;
 			if ((checker = contains(identifier)) != null &&
-					!checker.equals(identifier + identifier + " " + 
+					!checker.equals(identifier + " " + 
 							clientSocket.getInetAddress() + "\n")) { //IP for a user changed
 				System.out.println("IP for \"" + identifier + "\" has changed.");
-				System.out.println("Difference: " + contains(identifier) + " : " + clientSocket.getInetAddress() + "\n");
+				System.out.println("Difference: " + contains(identifier) + " : " + identifier + " " + clientSocket.getInetAddress() + "\n");
 				replace(identifier, identifier + " " + 
 						clientSocket.getInetAddress());
 			} else {
